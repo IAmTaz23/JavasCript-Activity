5829f4914a8b57727ce698252a46a78339750cf6
Brandon Barnett <bbarnett12h@gmail.com>
2023-10-18 14:57:03 -0400

    javascript activity

---
+++ activityscript.js
@@ -0,0 +1,47 @@
+const firstName = "Brandon";
+const lastName = "Barnett";
+
+console.log("Greetings", firstName, lastName)
+
+const x = 5 
+const y = 10
+console.log(x+y)
+
+const z = x+y
+console.log(z)
+
+const c_degrees = 20;
+const f_degrees = [c_degrees] *(9/5) + 32;
+console.log(f_degrees)
+
+let a = 2;
+console.log(a)
+a -= 1;
+console.log(a)
+a--;
+console.log(a)
+a += 4;
+console.log(a)
+
+a++;
+console.log(a)
+let b = 8;
+console.log(b)
+
+
+const c = a + b * b;
+console.log(c)
+
+const d = a * b + b;
+console.log(d)
+
+const e = a * (b + b);
+console.log(e)
+
+const f = a * b / a;
+console.log(f)
+
+const g = b / a * a ;
+console.log(g)
+
+
---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Hello World!")
+
+
+//Variables
+//Var
+//let
+//const
+
+// const x = 5;
+// const y = 6;
+// const z = x + y;
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+//string
+const firstName = "James";
+console.log(firstName)
+//Numbers
+const age = 30.5;
+console.log(age)
+//Boolean
+const isStudent = false;
+//undefined
+let x; //undefined declaration
+let y = 5 //initialized variable
+console.log(x)
